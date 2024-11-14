44209f6da0b5fa3087693054730e01f14aec9a08
Ahmed <aminedu957@gmail.com>
2024-11-14 15:49:22 +0200

    hello

---
+++ main.js
@@ -0,0 +1,12 @@
+// add class active on scrol 
+let header = document.querySelector('header')
+
+
+// show the menu on click
+let nav_links=document.getElementById('links')
+function open_close_menu(){
+
+    // Toggle a class that controls visibility
+    nav_links.classList.toggle('active');
+}
+
